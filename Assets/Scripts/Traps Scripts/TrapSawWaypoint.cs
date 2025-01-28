569fa4f39457ef6ed2547221ec40c28fbb81185f
loicrohan <loicrohan@gmail.com>
2025-01-28 17:44:53 +0530

    Another Cleanup

--- Assets/Scripts/Traps Scripts/TrapSawWaypoint.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TrapSawWaypoint : MonoBehaviour
-{
-    //needed to detect the object attached to this script
-}
